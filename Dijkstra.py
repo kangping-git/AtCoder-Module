6bf969f35dd2dfd265c39148d4a1eebbaa032c42
Kangping <69379510+kangping-git@users.noreply.github.com>
2025-06-28 20:30:31 +0900

    Create Dijkstra.py

---
+++ Dijkstra.py
@@ -0,0 +1,16 @@
+INF = 10 ** 18
+
+def dijkstra(n: int, adj: List[List[Tuple[int, int]]], src: int) -> List[int]:
+    dist = [INF] * n
+    dist[src] = 0
+    pq = [(0, src)]
+    while pq:
+        d, u = heapq.heappop(pq)
+        if d != dist[u]:
+            continue
+        for v, w in adj[u]:
+            nd = d + w
+            if nd < dist[v]:
+                dist[v] = nd
+                heapq.heappush(pq, (nd, v))
+    return dist
